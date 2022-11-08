5422017ae4c70db1a2eb60c6b2f1593f298ce3a0
Federico <vgfede@gmail.com>
2022-11-08 14:29:13 +0100

    fixed flask app creation

--- src/__init__.py
+++ src/__init__.py
@@ -1,16 +1,21 @@
 from flask import Flask
 import sqlite3 as sl
 
-app = Flask(__name__)
+def create_app():
+    app = Flask(__name__)
+    app.config.from_mapping(
+        SECRET_KEY='dev',
+    )
+    # Load db
+    db_disk = sl.connect('db_disk.db')
+    db_mem = sl.connect(':memory:')
+    db_disk.backup(db_mem)
+    print("DEBUG: Database loaded")
+    # db_mem.backup(db_disk) # write db to disk
+    return app
+
+app = create_app()
 
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
-
-if __name__ == "__main__":
-    db_disk = sl.connect('db_disk.db')
-    db_mem = sl.connect(':memory:')
-    db_disk.backup(db_mem)
-    app.run()
-    # ...
-    # db_mem.backup(db_disk) # write db to disk
