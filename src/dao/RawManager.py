883cc124b81643fe4aeb1bc5eb8d32499a22ebfb
Federico <vgfede@gmail.com>
2022-11-22 15:02:17 +0100

    raw data update

--- src/dao/RawManager.py
+++ src/dao/RawManager.py
@@ -19,16 +19,22 @@ class RawManager(Manager):
 
     @staticmethod
     def get_raw_data_by_time_interval_by_building(id_building, start_time, end_time):
-        raw_data = Raw.query.filter(and_(Raw.id_building == id_building, Raw.timestamp >= start_time, Raw.timestamp <= end_time)).all()
-        # raw_data = [raw_dict(raw) for raw in raw_data]
+        raw_data = Raw.query.filter(
+            and_(
+                Raw.id_building == id_building,
+                Raw.timestamp >= start_time,
+                Raw.timestamp <= end_time,
+            )
+        ).all()
+        raw_data = [raw_dict(raw) for raw in raw_data]
         return raw_data
+
 
 def raw_dict(raw):
     return {
         "id": raw.id,
+        "id_building": raw.id_building,
         "timestamp": raw.timestamp,
-        "mac": raw.mac,
-        "rssi_1": raw.rssi_1,
-        "rssi_2": raw.rssi_2,
-        "rssi_3": raw.rssi_3,
+        "mac_hash": raw.mac_hash,
+        "rssi_device": raw.rssi_device,
     }
--- src/routes/raw_data.py
+++ src/routes/raw_data.py
@@ -9,11 +9,10 @@ from datetime import datetime
 @app.route("/api/raw/push/")
 def push_raw():
     raw = Raw()
+    raw.id_building = 1
     raw.timestamp = datetime.now()
-    raw.mac = "macaddress"
-    raw.rssi_1 = 1
-    raw.rssi_2 = 2
-    raw.rssi_3 = 3
+    raw.mac_hash = "test_macaddr1234"
+    raw.rssi_device = [[1234, -50], [5678, -10], [9012, -20]]
     RawManager.add(raw)
     return "<p>Data pushed</p>"
 
@@ -23,7 +22,10 @@ def pull_raw():
     raws = RawManager.get_all()
     return raws
 
+
 @app.route("/api/raw/pull/<id_building>/<start_time>/<end_time>/")
 def pull_raw_by_time_interval(id_building, start_time, end_time):
-    n_raw_data = RawManager.get_raw_data_by_time_interval_by_building(id_building, start_time, end_time)
+    n_raw_data = RawManager.get_raw_data_by_time_interval_by_building(
+        id_building, start_time, end_time
+    )
     return str(len(n_raw_data))
